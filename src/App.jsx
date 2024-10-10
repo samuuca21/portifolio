cbe411f88f4f269d2602d932b086e66b642f2319
samuuca21 <samuelbrondani20@gmail.com>
2024-10-10 20:07:24 -0300

    Fazendo Deploy

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Pagina() {
+
+return <main>
+
+  <img src="./myself.jpg" alt="foto" width="128" />
+
+  <div>
+    <b>Samuel Rossetto Brondani</b> | 18 anos | | Fracassado, Pootis |
+  </div>
+  <div>
+    <b>Perfil</b>: Olá sou o Samuel, desenvolvedor web, artista e modelador.
+  </div>
+
+  <div>
+    <b>Experiencias</b>: Já fiz trabalhos em 3D e sistemas em Python.
+  </div>
+
+  <div>
+    <b>Competencias</b>:
+      Python,
+      HTML,
+      CSS,
+      Javascript,
+      3D,
+      Design,
+      computaria.
+  </div>
+
+  <div>
+    <a href="https://github.com/samuuca21">Github</a>
+  </div>
+
+
+
+
+</main>
+
+
+}
